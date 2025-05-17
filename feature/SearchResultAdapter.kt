0e0517cca5c484d4c55e4481c005e430ae09ba89
Jeiniy <144749348+Jeiniy@users.noreply.github.com>
2025-05-17 23:35:30 +0800

    呈現搜尋結果
    搭配 SearchActivity

--- feature/SearchResultAdapter.kt
+++ feature/SearchResultAdapter.kt
@@ -1,22 +1,22 @@
-package com.example.finalproject
+package com.example.smartfridgeassistant
 
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.finalproject.Food
 
 class SearchResultAdapter : RecyclerView.Adapter<SearchResultAdapter.ViewHolder>() {
 
-    private var foods = listOf<Food>()
-
-    fun updateData(newList: List<Food>) {
-        foods = newList
+    private var foods = listOf<FoodItem>()
+    fun updateData(newData: List<FoodItem>)  {
+        foods = newData
         notifyDataSetChanged()
     }
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val foodName: TextView = view.findViewById(R.id.food_name)
+        val foodName: TextView = view.findViewById(R.id.food)
         val expirationDate: TextView = view.findViewById(R.id.expiration_date)
     }
 
@@ -29,7 +29,7 @@ class SearchResultAdapter : RecyclerView.Adapter<SearchResultAdapter.ViewHolder>
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val item = foods[position]
         holder.foodName.text = item.name
-        holder.expirationDate.text = "到期日：${item.expirationDate}"
+        holder.expirationDate.text = "到期日：${item.expiryDate}"
     }
 
     override fun getItemCount(): Int = foods.size
